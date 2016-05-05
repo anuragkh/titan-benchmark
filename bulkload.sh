df11dd78e75c0318b05a1753c1eff84d893a14d8
Anurag Khandelwal <anurag.akcool@gmail.com>
2016-05-04 23:15:09 -0700

    Update bulkload.sh

--- bulkload.sh
+++ bulkload.sh
@@ -1,5 +1,5 @@
 export MAVEN_OPTS="-verbose:gc -server -Xmx50g"
 mvn clean compile
-sed -i -e 's/-Xmx256M/-Xmx8G/g' /usr/bin/sstableloader
+sudo sed -i -e 's/-Xmx256M/-Xmx8G/g' /usr/bin/sstableloader
 nodetool enablethrift
 mvn exec:java -Dexec.mainClass="edu.berkeley.cs.Load"
